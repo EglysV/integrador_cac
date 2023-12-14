afceea87df7b176e7cafc13ec0f1343ed7e6475a
EglysV <evm.1828@gmail.com>
2023-12-14 10:36:50 -0300

    Se agrega tpintegrador BD

---
+++ oradores.sql
@@ -0,0 +1,13 @@
+SELECT * FROM integrador_cac.oradores;
+
+INSERT INTO oradores (id_orador,nombre,apellido,mail,tema,fecha_alta) VALUES
+('01','Pedro', 'Sanchez','pedrosanchez@gmail.com','Java','2023-12-05'),
+('02','Pablo', 'Ramirez','pabloramirez@gmail.com','JavaScript','2023-12-06'),
+('03','Maria', 'Perez','mariaperez@gmail.com','NodeJs','2023-12-07'),
+('04','Celeste', 'Vasquez','celestevasquez@gmail.com','MySql','2023-12-08'),
+('05','Valeria', 'Paz','valeriapaz@gmail.com','BD','2023-12-09'),
+('06','Lola', 'Sanchez','lolasanchez@gmail.com','POO','2023-12-10'),
+('07','Lila', 'Rosas','lilarosas@gmail.com','Java','2023-12-11'),
+('08','Fidel', 'Fuentes','fidelfuentes@gmail.com','Desarrollo web','2023-12-12'),
+('09','Martin', 'Velez','martinvelez@gmail.com','Inicio en Tecnologia','2023-12-13'),
+('10','Sofia', 'Jimenez','sofiajimenez@gmail.com','Base de Datos','2023-12-14');
